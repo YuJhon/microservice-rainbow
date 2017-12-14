0905d77f68cfbe6681c3ce2e371cbe796e623eb0
YuJhon <3291371805@qq.com>
2017-12-14 09:04:11 +0800

    Model模块的主应用程序的引入

---
+++ ms-rainbow-eureka-model/src/main/java/com/jhon/rain/MsRainbowEurekaModelApplication.java
@@ -0,0 +1,20 @@
+package com.jhon.rain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>功能描述</br>模型模块的主应用程序的引入</p>
+ *
+ * @author jiangy19
+ * @version v1.0
+ * @projectName ms-rainbow
+ * @date 2017/12/14 9:01
+ */
+@SpringBootApplication
+public class MsRainbowEurekaModelApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MsRainbowEurekaModelApplication.class, args);
+  }
+}
